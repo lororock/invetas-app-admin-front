6e85e184a7cde901349f2350fea33428e675858b
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-11-27 15:56:57 -0500

    change style login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useForm, useField } from "vee-validate";
 import * as yup from "yup";
 import { useAuthStore } from "../store/auth.store.ts";
@@ -16,7 +17,7 @@ const schema = yup.object({
     .max(50, "Debe tener máximo de 50 caracteres")
     .matches(
       /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-      "El password debería tener una mayúscula, una minúscula y un numero",
+      "El password debería tener una mayúscula, una minúscula y un numero"
     ),
 });
 
@@ -37,38 +38,149 @@ const submitLogin = async () => {
   const isAuth = await authStore.validTokenRefresh();
   if (isAuth) await router.push("/form");
 })();
+
+const visible = ref(false);
+
+const togglePasswordVisibility = () => {
+  visible.value = !visible.value;
+  console.log("hola");
+};
 </script>
 
 <template>
-  <v-card class="max-w-2xl mx-auto mt-20">
-    <v-form @submit.prevent="submitLogin">
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <v-text-field
-              density="compact"
-              v-model="email"
-              :error-messages="emailError"
-              label="Correo"
-              required
-            />
-          </v-col>
+  <!--
+  Heads up! 👋
 
-          <v-col cols="12">
-            <v-text-field
-              density="compact"
-              v-model="password"
-              :error-messages="passwordError"
-              label="Contraseña"
-              required
-              type="password"
-            />
-          </v-col>
-        </v-row>
-        <v-btn :disabled="!meta.valid" block type="submit" class="bg-green">
-          Iniciar Sesión
-        </v-btn>
-      </v-container>
-    </v-form>
-  </v-card>
+  Plugins:
+    - @tailwindcss/forms
+-->
+
+  <section class="bg-white">
+    <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+      <section
+        class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
+      >
+        <img
+          alt="Night"
+          src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          class="absolute inset-0 h-full w-full object-cover opacity-80"
+        />
+
+        <div class="hidden lg:relative lg:block lg:p-12">
+          <a class="block text-white" href="/">
+            <span class="sr-only">Home</span>
+            <svg
+              class="h-8 sm:h-10"
+              viewBox="0 0 28 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+
+          <h2
+            class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl"
+          >
+            Inventas-App 💰
+          </h2>
+
+          <p class="mt-4 leading-relaxed text-white/90">
+            Sistema integral para la gestión eficiente de inventarios, ventas y
+            relaciones con clientes
+          </p>
+        </div>
+      </section>
+
+      <main
+        class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+      >
+        <div class="max-w-xl lg:max-w-3xl">
+          <div class="relative -mt-16 block lg:hidden">
+            <a
+              class="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
+              href="/"
+            >
+              <span class="sr-only">Home</span>
+              <svg
+                class="h-8 sm:h-10"
+                viewBox="0 0 28 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </a>
+
+            <h1
+              class="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
+            >
+              Inventas-App 💰
+            </h1>
+
+            <p class="mt-4 leading-relaxed text-gray-500">
+              Sistema integral para la gestión eficiente de inventarios, ventas
+              y relaciones con clientes
+            </p>
+          </div>
+
+          <v-form
+            @submit.prevent="submitLogin"
+            class="mt-8 grid grid-cols-6 gap-6"
+          >
+            <div class="col-span-6">
+              <v-text-field
+                density="compact"
+                v-model="email"
+                :error-messages="emailError"
+                label="Correo"
+                required
+                placeholder="Correo electronico"
+                prepend-inner-icon="mdi-email-outline"
+                variant="outlined"
+              />
+            </div>
+
+            <div class="col-span-6">
+              <v-text-field
+                :error-messages="passwordError"
+                label="Contraseña"
+                required
+                v-model="password"
+                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="visible ? 'text' : 'password'"
+                density="compact"
+                placeholder="Contrasena"
+                prepend-inner-icon="mdi-lock-outline"
+                variant="outlined"
+                @click:append-inner="togglePasswordVisibility"
+              />
+            </div>
+
+            <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
+              <button
+                :disabled="!meta.valid"
+                block
+                type="submit"
+                class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition focus:outline-none focus:ring active:text-blue-500"
+              >
+                Iniciar sesión
+              </button>
+
+              <p class="mt-4 text-sm text-gray-500 sm:mt-0">
+                Already have an account?
+                <a href="#" class="text-gray-700 underline">Log in</a>.
+              </p>
+            </div>
+          </v-form>
+        </div>
+      </main>
+    </div>
+  </section>
 </template>
